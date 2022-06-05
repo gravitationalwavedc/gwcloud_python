9403be695f1535808b2e53d18395c096114a9c38
TomReichardt <thomas.reichardt@students.mq.edu.au>
2022-06-05 23:37:20 +0000

    Extracting case switching and adding Job metaclass
    Removed all the snake_case and camelCase switching, extracting to gwdc_python with pyhumps. These packages should now only deal with snake_case, except for IN queries (but I will address this at some point for consistency).
    
    Also added a job meta and base class for uniformity and to extract the dynamic method creation.
    
    Approved-by: Lewis Lakerink <llakerink@swin.edu.au>
    
    Merged by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwcloud_python/-/merge_requests/8

--- gwcloud_python/utils/__init__.py
+++ gwcloud_python/utils/__init__.py
@@ -1,8 +1,8 @@
 from .utils import (
-    to_snake_case,
-    to_camel_case,
+    # to_snake_case,
+    # to_camel_case,
     rename_dict_keys,
-    convert_dict_keys,
+    # convert_dict_keys,
 )
 from . import identifiers
 from . import file_filters
