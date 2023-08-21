e709d81aa328c446360dca2b293a5d5d07811a70
TomReichardt <thomas.reichardt@students.mq.edu.au>
2023-08-21 13:55:40 +1000

    Update settings, bump

--- gwcloud_python/utils/tests/test_file_upload.py
+++ gwcloud_python/utils/tests/test_file_upload.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 def test_get_endpoint_from_uploaded():
     with TemporaryDirectory() as tmp_dir:
-        test_file = NamedTemporaryFile(dir=tmp_dir)
-        try:
-            file_name = check_file(test_file.name)
-            assert file_name == Path(test_file.name)
-        except Exception:
-            pytest.fail("check_file is failing when it shouldn't")
+        with NamedTemporaryFile(dir=tmp_dir) as test_file:
+            try:
+                file_name = check_file(test_file.name)
+                assert file_name == Path(test_file.name)
+            except Exception:
+                pytest.fail("check_file is failing when it shouldn't")
 
         with pytest.raises(Exception):
             check_file(tmp_dir / 'nonexistant')
