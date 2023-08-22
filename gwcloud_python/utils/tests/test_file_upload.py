b8fcf0aedebf4b6acbeb3ac1206dc751cfd3d65e
TomReichardt <thomas.reichardt@students.mq.edu.au>
2023-08-22 11:14:28 +1000

    Remove try-catch from test

--- gwcloud_python/utils/tests/test_file_upload.py
+++ gwcloud_python/utils/tests/test_file_upload.py
@@ -7,11 +7,8 @@ from pathlib import Path
 def test_get_endpoint_from_uploaded():
     with TemporaryDirectory() as tmp_dir:
         with NamedTemporaryFile(dir=tmp_dir) as test_file:
-            try:
-                file_name = check_file(test_file.name)
-                assert file_name == Path(test_file.name)
-            except Exception:
-                pytest.fail("check_file is failing when it shouldn't")
+            file_name = check_file(test_file.name)
+            assert file_name == Path(test_file.name)
 
         with pytest.raises(Exception):
             check_file(tmp_dir / 'nonexistant')
