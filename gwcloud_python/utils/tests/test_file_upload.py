69d1d996551f89cbf813c1efb955ade1a2ebaf6c
TomReichardt <thomas.reichardt@students.mq.edu.au>
2023-08-22 01:35:40 +0000

    Update settings, bump
    Updating settings to be using general gwdc settings for file download endpoint
    
    Approved-by: Lewis Lakerink <llakerink@swin.edu.au>
    
    Merged by: TomReichardt <thomas.reichardt@students.mq.edu.au>
    
    See merge request https://gitlab.com/CAS-eResearch/GWDC/code/gwcloud_python/-/merge_requests/17

--- gwcloud_python/utils/tests/test_file_upload.py
+++ gwcloud_python/utils/tests/test_file_upload.py
@@ -6,12 +6,9 @@ from pathlib import Path
 
 def test_get_endpoint_from_uploaded():
     with TemporaryDirectory() as tmp_dir:
-        test_file = NamedTemporaryFile(dir=tmp_dir)
-        try:
+        with NamedTemporaryFile(dir=tmp_dir) as test_file:
             file_name = check_file(test_file.name)
             assert file_name == Path(test_file.name)
-        except Exception:
-            pytest.fail("check_file is failing when it shouldn't")
 
         with pytest.raises(Exception):
             check_file(tmp_dir / 'nonexistant')
